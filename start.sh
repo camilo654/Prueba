78c77cf8e1c316f01ead54a2f68bab9b071b6208
dfhincapiem <dfhincapiem@unal.edu.co>
2017-10-04 23:18:37 -0500

    se arregla el start.sh con seed

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 docker-compose build
 docker-compose run --rm web rails db:create
-docker-compose run --rm web rails db:seed
+
 docker-compose run --rm web rails db:migrate
+docker-compose run --rm web rails db:seed
 docker-compose up
