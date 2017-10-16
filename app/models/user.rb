96bc24fa7f670513ef37f0c3a79e94a107045b2c
Camilo Peña <capenap@unal.edu.co>
2017-10-16 12:07:32 -0500

    Domicilio creado automaticamente cuando se crea un usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
 	belongs_to :usuario
     has_one :domicile, dependent: :destroy
+
+    before_create :build_default_domicile
+    
+    def build_default_domicile
+        build_domicile
+        true
+    end
 end
