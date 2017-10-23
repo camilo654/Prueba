933db075a9cc5bf0dcd05565d54898ba6bdca9ad
CCTORRESR <cctorresr@unal.edu.co>
2017-10-23 14:32:33 -0500

    Codigo de prueba de consumo

--- app/models/outlet.rb
+++ app/models/outlet.rb
@@ -7,21 +7,22 @@ class Outlet < ApplicationRecord
     private
     	def guardar_registro
     		if self.state?
-    			Record.create(household_appliances: 'id_electro', state: true)
+    			Record.create(household_appliances: household_apliance.id, state: true)
     		else
-    			Record.create(household_appliances: 'id_electro', state: nil)
-    			:calcula_consumo(id_electro)
+    			Record.create(household_appliances: household_apliance.id, state: nil)
+    			:calcula_consumo
     		end
     	end
 
-    	def calcula_consumo(id)
-    		#Va a la tabla Record y con el id busca el ultimo registro con state = 0 y 
-    		#el anterior registro con state = 1
-    		#valIni = registro.state1
-    		#valFin = registro.state0
-    		#Resta entre fechas para obtener tiempo que estuvo encendido y multiplicar por lo que consuma el electro
-    		#consumo = (valFin - valIni) * Household_appliance.getElectricity_use(id)
-    		Register.create(id: id,date: CURDATE(), consumption: consumo)
+    	def calcula_consumo
+            #Va a la tabla Record y con el id busca el ultimo registro con state = 0
+            @valIni = Record.where("household_appliances = ? and state = ?", household_apliance.id, 0).last;
+            #Va a la tabla Record y con el id busca el ultimo registro con state = 1
+            @valFin = Record.where("household_appliances = ? and state = ?", household_apliance.id, 1).last;
+            #Resta entre fechas para obtener tiempo que estuvo encendido y multiplicar por lo que consuma el electro
+    		@consumo = (valFin - valIni) * household_apliance.electricity_use;
+            @time = Time.now
+    		Register.create(household_appliance_id: household_apliance.id, date: t.strftime("%d/%m/%Y"), consumption: consumo)
     	end
 
 end
--- db/migrate/20171016234017_create_records.rb
+++ db/migrate/20171016234017_create_records.rb
@@ -2,7 +2,7 @@ class CreateRecords < ActiveRecord::Migration[5.1]
   def change
     create_table :records do |t|
       t.datetime :time, :null => false, :default => Time.now
-      t.tinyint :household_appliances
+      t.integer :household_appliances
       t.boolean :state
       t.belongs_to :outlet, index: true, foreign_key: true
 
