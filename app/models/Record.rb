14c0dd07b49ae1c962ef73fe5b766184c47bcf0e
CCTORRESR <cctorresr@unal.edu.co>
2017-10-28 17:07:10 -0500

    Consumo

--- app/models/Record.rb
+++ app/models/Record.rb
@@ -1,3 +1,6 @@
 class Record < ApplicationRecord
     belongs_to :outlet
+    def self.by_outlet( outlet_id )
+    	load.where( {outlet_id: outlet_id} )
+  	end
 end
--- app/models/outlet.rb
+++ app/models/outlet.rb
@@ -4,25 +4,33 @@ class Outlet < ApplicationRecord
 
     after_save :guardar_registro, on: :update
 
+    @household_apliance = Household_apliance.by_id(self.household_apliance_id)
+    @records = Record.by_record(self.by_outlet)
+
     private
-    	def guardar_registro
+    	def guardar_registro            
+
     		if self.state?
-    			Record.create(household_appliances: household_apliance.id, state: true)
+    			Record.create(household_appliances: household_apliance_id, state: true)
     		else
-    			Record.create(household_appliances: household_apliance.id, state: nil)
+    			Record.create(household_appliances: household_apliance_id, state: nil)
     			:calcula_consumo
     		end
     	end
 
     	def calcula_consumo
             #Va a la tabla Record y con el id busca el ultimo registro con state = 0
-            @valIni = Record.where("household_appliances = ? and state = ?", household_apliance.id, 0).last;
+            @valIni = Record.where("household_appliances = ? and state = ?", household_apliance_id, 0).last;
             #Va a la tabla Record y con el id busca el ultimo registro con state = 1
-            @valFin = Record.where("household_appliances = ? and state = ?", household_apliance.id, 1).last;
+            @valFin = Record.where("household_appliances = ? and state = ?", household_apliance_id, 1).last;
             #Resta entre fechas para obtener tiempo que estuvo encendido y multiplicar por lo que consuma el electro
-    		@consumo = (valFin - valIni) * household_apliance.electricity_use;
+    		@consumo = (valFin - valIni) * @household_apliance.electricity_use);
             @time = Time.now
     		Register.create(household_appliance_id: household_apliance.id, date: t.strftime("%d/%m/%Y"), consumption: consumo)
     	end
+
+        def self.by_light( light_id )
+            load.where( {light_id: light_id} )
+        end
 
 end
